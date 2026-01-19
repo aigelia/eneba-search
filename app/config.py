5850af0cc05f094aae66a98d4d33060f3a708359
Katerina <e.mayakovskaya@gmail.com>
2026-01-19 22:46:28 +0200

    Add app config

---
+++ app/config.py
@@ -0,0 +1,42 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+env_file = os.path.join(PROJECT_ROOT, ".env")
+
+
+class DatabaseConfig(BaseSettings):
+    host: str
+    port: int
+    db: str
+    user: str
+    password: str
+
+    model_config = SettingsConfigDict(
+        env_file=env_file,
+        env_file_encoding="utf-8",
+        extra="ignore",
+        env_prefix="POSTGRES_",
+    )
+
+    @property
+    def postgres_url(self) -> str:
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.db}"
+
+
+class AppConfig(BaseSettings):
+    debug: bool = True
+
+    model_config = SettingsConfigDict(
+        env_file=env_file,
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+
+class Settings(BaseSettings):
+    app: AppConfig = AppConfig()  # type: ignore
+    database: DatabaseConfig = DatabaseConfig()  # type: ignore
+
+
+settings = Settings()
